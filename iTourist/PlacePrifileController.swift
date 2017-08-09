4d58d75d4b32b3a44215cfe6a174f4350836cc02
Zhanna Moskaliuk <octoberrock11@gmail.com>
2017-08-09 18:35:05 +0300

    Fixed destination of segue

--- iTourist/PlacePrifileController.swift
+++ iTourist/PlacePrifileController.swift
@@ -11,6 +11,12 @@ import UIKit
 class PlacePrifileController: UIViewController {
     var place: Place?
     
+    @IBOutlet weak var phoneNumLabel: UILabel!
+    @IBOutlet weak var adressLabel: UILabel!
+    @IBOutlet weak var workHourLabel: UILabel!
+    @IBOutlet weak var websiteLabel: UILabel!
+    @IBOutlet weak var feedbackButton: UIButton!
+    @IBOutlet weak var nameNavigationItem: UINavigationItem!
     
     @IBAction func closePlaceProfile(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
@@ -18,6 +24,19 @@ class PlacePrifileController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        if let plc = place {
+            nameNavigationItem.title = plc.name
+            adressLabel.text = plc.formattedAddress
+            phoneNumLabel.text = plc.internationalPhoneNumber
+            
+            if let work = plc.openingHours {
+                workHourLabel.text = (work[0] + "\n" + work[1] + "\n" + work[2] + "\n" + work[3] + "\n" + work[4] + "\n" + work[5] + "\n" + work[6])
+            }
+            
+            websiteLabel.text = plc.website?.absoluteString
+            feedbackButton.titleLabel?.text = "Feedbacks (\(plc.placeReviews?.count))"
+        }
     }
 }
