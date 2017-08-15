ca672ab92b06bcb7aee4ac735997fefde1edd757
yevhenRoman <yevhen_54@hotmail.com>
2017-08-15 12:54:49 +0300

    Changed PlaceCoreData

---
+++ iTourist/UserCoreData.swift
@@ -0,0 +1,9 @@
+//
+//  UserCoreData.swift
+//  iTourist
+//
+//  Created by Yevhen Roman on 8/15/17.
+//  Copyright © 2017 Lv-249iOS. All rights reserved.
+//
+
+import Foundation
---
+++ iTourist/UserStruct.swift
@@ -0,0 +1,16 @@
+//
+//  UserStruct.swift
+//  iTourist
+//
+//  Created by Yevhen Roman on 8/15/17.
+//  Copyright © 2017 Lv-249iOS. All rights reserved.
+//
+
+import Foundation
+
+struct User {
+    let name: String = ""
+    var surname: String = ""
+    var email: String = ""
+    var password: String = ""
+}
