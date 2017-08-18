9fd9367d4fe51fff4ecbb710b526ffe088e36584
yevhenRoman <yevhen_54@hotmail.com>
2017-08-18 12:47:33 +0300

    Added folder dashboard and class for iconSetting

---
+++ iTourist/DashboardSettingIcon.swift
@@ -0,0 +1,21 @@
+//
+//  DashboardSettingIcon.swift
+//  iTourist
+//
+//  Created by Yevhen Roman on 8/18/17.
+//  Copyright © 2017 Lv-249iOS. All rights reserved.
+//
+
+import UIKit
+
+class DashboardSettingIcon: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
