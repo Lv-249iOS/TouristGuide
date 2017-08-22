a8720349cea19d98215b4fdd1e578cf007f1f645
Kristina Del Rio Albrechet <kalbrechet@gmail.com>
2017-08-22 20:30:53 +0300

    Fixed bug with scroll view frame

--- iTourist/SettingsViewController.swift
+++ iTourist/SettingsViewController.swift
@@ -8,6 +8,14 @@
 
 import UIKit
 
+enum CellTags: Int {
+    case sound = 1
+    case allowUseLocation = 2
+    case changePassword = 3
+    case sortPlacesByName = 4
+    case useCelsius = 5
+}
+
 class SettingsViewController: UITableViewController {
     
     @IBOutlet weak var imageScroll: UIScrollView!
@@ -23,16 +31,28 @@ class SettingsViewController: UITableViewController {
     
     func fillImageScrollView() {
         for i in 0 ..< styleManager.backgroundThemeArray.count {
-            let imageView = UIImageView()
-            imageView.image = styleManager.backgroundThemeArray[i]
-            imageView.contentMode = .scaleToFill
-            imageView.frame = CGRect(x: view.frame.width * CGFloat(i),
-                                     y: 0,
-                                     width: imageScroll.frame.width,
-                                     height: imageScroll.frame.height)
+            let rect = CGRect(x: imageScroll.bounds.width * CGFloat(i),
+                              y: 0,
+                              width: imageScroll.frame.width,
+                              height: imageScroll.frame.height)
             
-            imageScroll.contentSize.width = imageScroll.frame.width * CGFloat(i + 1)
+            let imageView = UIImageView(frame: rect)
+            imageView.contentMode = .scaleAspectFill
+            imageView.image = styleManager.backgroundThemeArray[i]
+            
+            imageScroll.contentSize.width = imageScroll.bounds.width * CGFloat(i + 1)
             imageScroll.addSubview(imageView)
+        }
+    }
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        for i in 0 ..< imageScroll.subviews.count {
+            let rect = CGRect(x: size.width * CGFloat(i),
+                              y: 0,
+                              width: size.width,
+                              height: imageScroll.frame.height)
+            imageScroll.subviews[i].frame = rect
+            imageScroll.contentSize.width = size.width * CGFloat(i + 1)
         }
     }
     
@@ -57,10 +77,33 @@ class SettingsViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        } else if tableView.cellForRow(at: indexPath)?.accessoryType == UITableViewCellAccessoryType.none {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        guard let cell = tableView.cellForRow(at: indexPath) else { return }
+        
+        if cell.accessoryType == .checkmark {
+            cell.accessoryType = .none
+            notifyAboutChanges(with: cell.tag, value: false)
+        } else if cell.accessoryType == UITableViewCellAccessoryType.none {
+            cell.accessoryType = .checkmark
+            notifyAboutChanges(with: cell.tag, value: true)
+        }
+    }
+    
+    func notifyAboutChanges(with cellTag: Int, value: Bool) {
+        switch  cellTag {
+        case CellTags.sound.rawValue:
+            UserDefaults.standard.set(value, forKey: PathForSettingsKey.sound.rawValue)
+            NotificationCenter.default.post(name: Constants.isMakeSound, object: nil)
+        case CellTags.allowUseLocation.rawValue:
+            UserDefaults.standard.set(value, forKey: PathForSettingsKey.userLocation.rawValue)
+        case CellTags.sortPlacesByName.rawValue:
+            UserDefaults.standard.set(value, forKey: PathForSettingsKey.sortPlaces.rawValue)
+        case CellTags.useCelsius.rawValue:
+            UserDefaults.standard.set(value, forKey: PathForSettingsKey.celcius.rawValue)
+        default:
+            break
         }
     }
 }
+
+
+
