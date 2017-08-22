bc591491487c99f4628e125240f2c570a2df4fe1
Kristina Del Rio Albrechet <kalbrechet@gmail.com>
2017-08-22 12:07:24 +0300

    Deleted warnings with rounding buttons, added image scroll

--- iTourist/SettingsViewController.swift
+++ iTourist/SettingsViewController.swift
@@ -8,21 +8,35 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {
+class SettingsViewController: UITableViewController {
 
+    @IBOutlet weak var imageScroll: UIScrollView!
+    
+    let backgroundThemeArray = [#imageLiteral(resourceName: "background"), #imageLiteral(resourceName: "back"), #imageLiteral(resourceName: "profileBackground")]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        for i in 0 ..< backgroundThemeArray.count {
+            let imageView = UIImageView()
+            imageView.image = backgroundThemeArray[i]
+            imageView.contentMode = .scaleToFill
+            imageView.frame = CGRect(x: view.frame.width * CGFloat(i),
+                                     y: 0,
+                                     width: imageScroll.frame.width,
+                                     height: imageScroll.frame.height)
+            
+            imageScroll.contentSize.width = imageScroll.frame.width * CGFloat(i + 1)
+            imageScroll.addSubview(imageView)
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         self.navigationController?.isNavigationBarHidden = false
+    }
+    
+    override func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        scrollView.scrollRectToVisible(scrollView.subviews[2].bounds, animated: true)
     }
 }
